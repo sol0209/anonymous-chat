6baee2c53113413c257b7a18999f5b87214bf731
sol0209 <jinsolp0209@naver.com>
2025-05-16 19:42:43 +0900

    Update server.js

--- server.js
+++ server.js
@@ -74,8 +74,3 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on ${PORT}`));
-
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running on ${PORT}`));
-
